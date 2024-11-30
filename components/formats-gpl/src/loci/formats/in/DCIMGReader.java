045a760e34d1a4ef4943275dcd4b1e15a110605a
zacsimile <zachcm@gmail.com>
2024-11-30 18:38:25 +0100

    Working multifile import

--- components/formats-gpl/src/loci/formats/in/DCIMGReader.java
+++ components/formats-gpl/src/loci/formats/in/DCIMGReader.java
@@ -1,7 +1,13 @@
 package loci.formats.in;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import com.sun.media.jfxmedia.logging.Logger;
+
+import loci.common.Location;
 import loci.common.RandomAccessInputStream;
 import loci.formats.CoreMetadata;
 import loci.formats.FormatException;
@@ -17,145 +23,262 @@ import loci.formats.meta.MetadataStore;
  */
 public class DCIMGReader extends FormatReader {
 
-    // -- Constants --
+  // -- Constants --
 
-    private static final boolean IS_LITTLE = true;
+  private static final boolean IS_LITTLE = true;
 
-    private static final long DCAM_VERSION_0 = 0x7;
-    private static final long DCAM_VERSION_1 = 0x1000000;
+  private static final long DCAM_VERSION_0 = 0x7;
+  private static final long DCAM_VERSION_1 = 0x1000000;
 
-    private static final long DCIMG_PIXELTYPE_NONE = 0x00000000; // defined, but I don't know what to do with this
-    private static final long DCIMG_PIXELTYPE_MONO8 = 0x00000001;
-    private static final long DCIMG_PIXELTYPE_MONO16 = 0x00000002;
+  private static final long DCIMG_PIXELTYPE_NONE = 0x00000000; // defined, but I don't know what to do with this
+  private static final long DCIMG_PIXELTYPE_MONO8 = 0x00000001;
+  private static final long DCIMG_PIXELTYPE_MONO16 = 0x00000002;
 
-    // -- Fields --
+  // -- Fields --
 
-    private long sessionOffset;
-    private long dataOffset;
-    private long pixelType;
-    private int byteFactor;
+  private long sessionOffset;
+  private long dataOffset;
+  private long pixelType;
+  private int byteFactor;
 
-    public DCIMGReader() {
-        super("Hamamatsu DCIMG", "dcimg");
-        suffixSufficient = false;
-        domains = new String[] {FormatTools.UNKNOWN_DOMAIN};
+  private List<String> companionFiles = new ArrayList<String>();
+  private String[] uniqueFiles;
+
+  public DCIMGReader() 
+  {
+    super("Hamamatsu DCIMG", "dcimg");
+    suffixSufficient = false;
+    domains = new String[] {FormatTools.UNKNOWN_DOMAIN};
+  }
+
+  @Override
+  public boolean isSingleFile(String id) 
+    throws FormatException, IOException 
+  {
+    return false;
+  }
+
+  @Override
+  public int fileGroupOption(String id) 
+    throws FormatException, IOException 
+  {
+    return FormatTools.CAN_GROUP;
+  }
+
+  @Override
+  public String[] getSeriesUsedFiles(boolean noPixels) 
+  {
+    FormatTools.assertId(currentId, true, 1);
+
+    if (!isGroupFiles() || noPixels) return null;
+
+    return uniqueFiles;
+
+  }
+
+  @Override
+  public boolean isThisType(RandomAccessInputStream stream) 
+    throws IOException 
+  {
+    String desc = stream.readString(5);
+    if (!desc.equals("DCIMG")) return false;
+    return true;
+  }
+  
+  @Override
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h) 
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+
+    RandomAccessInputStream stream = new RandomAccessInputStream(uniqueFiles[no]);
+
+    // DCIMG is stored column major
+    stream.seek(sessionOffset + dataOffset + byteFactor*y*getSizeX());
+    for (int row=h-1; row>=0; row--) {
+      stream.skipBytes(byteFactor*x);
+      stream.read(buf, byteFactor*row*w, byteFactor*w);
+      stream.skipBytes(byteFactor*(getSizeX() - w - x));
     }
 
-    @Override
-    public boolean isThisType(RandomAccessInputStream stream) throws IOException {
-        String desc = stream.readString(5);
-        if (!desc.equals("DCIMG")) return false;
-        return true;
-    }
-    
-    @Override
-    public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h) 
-        throws FormatException, IOException
-    {
-        FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+    return buf;
+  }
 
-        // DCIMG is stored column major
-        in.seek(sessionOffset + dataOffset + byteFactor*y*getSizeX());
-        for (int row=h-1; row>=0; row--) {
-            in.skipBytes(byteFactor*x);
-            in.read(buf, byteFactor*row*w, byteFactor*w);
-            in.skipBytes(byteFactor*(getSizeX() - w - x));
-        }
+  @Override
+  protected void initFile(String id)
+    throws FormatException, IOException
+  {
+    super.initFile(id);
+    in = new RandomAccessInputStream(id);
 
-        return buf;
+    in.order(IS_LITTLE);  // little endian 
+
+    // confirm this is DCIMG
+    if (!isThisType(in)) throw new FormatException("Not a valid DCIMG file.");
+
+    in.seek(8);
+
+    long version = in.readUnsignedInt();  // DCIMG version number
+    if ((!(version == DCAM_VERSION_0)) && (!(version >= DCAM_VERSION_1))) {
+      throw new FormatException(String.format("Unknown DCIMG version number %d.", version));
     }
 
-    @Override
-    protected void initFile(String id) throws FormatException, IOException {
-        super.initFile(id);
-        in = new RandomAccessInputStream(id);
-
-        in.order(IS_LITTLE);  // big endian 
-
-        // confirm this is DCIMG
-        String desc = in.readString(5);
-        if (!desc.equals("DCIMG")) throw new FormatException("Not a valid DCIMG file.");
-
-        in.skipBytes(3);
-
-        long version = in.readUnsignedInt();  // DCIMG version number
-        if ((!(version == DCAM_VERSION_0)) && (!(version == DCAM_VERSION_1))) {
-            throw new FormatException(String.format("Unknown DCIMG version number %d.", version));
-        }
-
-        in.skipBytes(20);
-
-        long numSessions = in.readUnsignedInt();
-        long numFrames = in.readUnsignedInt();
-        sessionOffset = in.readUnsignedInt();
-        in.skipBytes(4);
-        long fileSize = in.readUnsignedInt();
-        in.skipBytes(12);
-        long fileSize2 = in.readUnsignedInt();
-        if (fileSize != fileSize2) throw new FormatException("Improper header. File sizes do not match.");
-        in.skipBytes(16);
-        long mystery1 = in.readUnsignedInt();  // 1024 in all examples
-
-        CoreMetadata m = core.get(0);
-
-        m.dimensionOrder = "XYZCT";
-        m.rgb = false;
-        m.interleaved = false;
-        m.littleEndian = IS_LITTLE;
-        m.indexed = false;
-        m.falseColor = false;
-        m.metadataComplete = true;
-        m.thumbnail = false;
-        m.sizeC = 1;
-        m.sizeZ = 1;
-        m.imageCount = getSizeZ() * getSizeC();
-
-        if (version == DCAM_VERSION_0) {
-            parseDCAMVersion0Header();
-        } else if (version == DCAM_VERSION_1) {
-            parseDCAMVersion1Header();
-        }
-
-        if (pixelType == DCIMG_PIXELTYPE_MONO8) {
-            m.pixelType = FormatTools.UINT8;
-            byteFactor = 1;
-        } else if (pixelType == DCIMG_PIXELTYPE_MONO16) {
-            m.pixelType = FormatTools.UINT16;
-            byteFactor = 2;
-        }
-
-        addGlobalMeta("Version", version);
-
-        // The metadata store we're working with.
-        MetadataStore store = makeFilterMetadata();
-        MetadataTools.populatePixels(store, this);
-
+    if (version > DCAM_VERSION_1) {
+      LOGGER.warn(String.format("Your file is DCAM version %d, but only %d is guaranteed to work.", version, DCAM_VERSION_1));
     }
 
-    private void parseDCAMVersion0Header() throws IOException {
-        
+    in.skipBytes(20);
+
+    long numSessions = in.readUnsignedInt();
+    long numFrames = in.readUnsignedInt();
+    sessionOffset = in.readUnsignedInt();
+    in.skipBytes(4);
+    long fileSize = in.readUnsignedInt();
+    in.skipBytes(12);
+    long fileSize2 = in.readUnsignedInt();
+    if (fileSize != fileSize2) throw new FormatException("Improper header. File sizes do not match.");
+    in.skipBytes(16);
+    long mystery1 = in.readUnsignedInt();  // 1024 in all examples
+
+    CoreMetadata m = core.get(0);
+
+    m.dimensionOrder = "XYZCT";
+    m.rgb = false;
+    m.interleaved = false;
+    m.littleEndian = IS_LITTLE;
+    m.indexed = false;
+    m.falseColor = false;
+    m.metadataComplete = true;
+    m.thumbnail = false;
+    m.sizeC = 1;
+
+    if (version == DCAM_VERSION_0) {
+      parseDCAMVersion0Header(in);
+    } else if (version == DCAM_VERSION_1) {
+      parseDCAMVersion1Header(in);
     }
 
-    private void parseDCAMVersion1Header() throws IOException {
-        CoreMetadata m = core.get(0);
-
-        in.seek(sessionOffset);
-        long sessionLength = in.readUnsignedInt();
-        in.skipBytes(20);
-        long pseudoOffset = in.readUnsignedInt(); 
-        in.skipBytes(32);  // unknown numbers 1, 144, 65537
-        m.sizeT = (int) in.readUnsignedInt();
-        pixelType = in.readUnsignedInt();
-        long mystery1 = in.readUnsignedInt();
-        // TODO: Is this casting always legal? I think not...
-        m.sizeX = (int) in.readUnsignedInt();  // num columns (this is a column-major format)
-        m.sizeY = (int) in.readUnsignedInt();  // num rows
-        long bytesPerRow = in.readUnsignedInt();
-        long bytesPerImage = in.readUnsignedInt();
-        in.skipBytes(8);
-        dataOffset = in.readUnsignedInt();
-        in.skipBytes(16);
-        long bytesPerFrame = in.readUnsignedInt();
-
+    if (pixelType == DCIMG_PIXELTYPE_MONO8) {
+      m.pixelType = FormatTools.UINT8;
+      byteFactor = 1;
+    } else if (pixelType == DCIMG_PIXELTYPE_MONO16) {
+      m.pixelType = FormatTools.UINT16;
+      byteFactor = 2;
     }
+
+    if (isGroupFiles()) {
+      Location currentFile = new Location(id).getAbsoluteFile();
+      Location directory = currentFile.getParentFile();
+      scanDirectory(directory);
+    } else {
+      String file = new Location(id).getAbsolutePath();
+      companionFiles.add(file);
+    }
+
+    uniqueFiles = companionFiles.toArray(new String[companionFiles.size()]);
+    m.sizeZ = uniqueFiles.length;
+    m.imageCount = getSizeZ() * getSizeC();
+
+    LOGGER.debug("sizeX: {} sizeY: {} sizeZ: {} sizeC: {} sizeT: {}", m.sizeX, m.sizeY, m.sizeZ, m.sizeC, m.sizeT);
+
+    addGlobalMeta("Version", version);
+
+    // The metadata store we're working with.
+    MetadataStore store = makeFilterMetadata();
+    MetadataTools.populatePixels(store, this);
+
+    in.close();
+
+  }
+
+  /* Logic copied from DicomReader */
+  private void scanDirectory(Location dir)
+    throws FormatException, IOException 
+  {
+    String[] files = dir.list(true);
+    if (files == null) return;
+    Arrays.sort(files);
+    for (String f : files) {
+      String file = new Location(dir, f).getAbsolutePath();
+      LOGGER.debug("Checking file {}", file);
+      addFileToList(file);
+    }
+  }
+
+  private void addFileToList(String file)
+    throws FormatException, IOException
+  {
+    String ext = getExtension(file);
+    if (!ext.equals("dcimg")) {
+      LOGGER.debug("File {} with extension {} failed extension check", file, ext);
+      return;
+    }
+    RandomAccessInputStream stream = new RandomAccessInputStream(file);
+    if (!isThisType(stream)) {
+      LOGGER.debug("File {} is not DCIMG", file);
+      stream.close();
+      return;
+    }
+    // stream.order(IS_LITTLE);
+    // now check width/height
+    companionFiles.add(file);
+    stream.close();
+  }
+
+  private String getExtension(String file)
+  {
+    String ext = "";
+    int i = file.lastIndexOf(".");
+    if (i > 0) {
+      ext = file.substring(i+1);
+    }
+    return ext;
+  }
+
+  private void parseDCAMVersion0Header(RandomAccessInputStream stream)
+    throws IOException 
+  {
+    CoreMetadata m = core.get(0);
+
+    stream.seek(sessionOffset);
+    long sessionLength = stream.readUnsignedInt();
+    stream.skipBytes(4);
+    long pseudoOffset = stream.readUnsignedInt(); 
+    stream.skipBytes(20);  
+    m.sizeT = (int) stream.readUnsignedInt();
+    pixelType = stream.readUnsignedInt();
+    long mystery1 = stream.readUnsignedInt();
+    // TODO: This will fail for large numbers
+    m.sizeX = (int) stream.readUnsignedInt();  // num columns (this is a column-major format)
+    long bytesPerRow = stream.readUnsignedInt();
+    m.sizeY = (int) stream.readUnsignedInt();  // num rows
+    long bytesPerImage = stream.readUnsignedInt();
+    stream.skipBytes(8);
+    dataOffset = stream.readUnsignedInt();
+    long offsetToFooter = stream.readUnsignedInt();  /// TODO: Deal with footer
+  }
+
+  private void parseDCAMVersion1Header(RandomAccessInputStream stream)
+    throws IOException 
+  {
+    CoreMetadata m = core.get(0);
+
+    stream.seek(sessionOffset);
+    long sessionLength = stream.readUnsignedInt();
+    stream.skipBytes(20);
+    long pseudoOffset = stream.readUnsignedInt(); 
+    stream.skipBytes(32);  // unknown numbers 1, 144, 65537
+    m.sizeT = (int) stream.readUnsignedInt();
+    pixelType = stream.readUnsignedInt();
+    long mystery1 = stream.readUnsignedInt();
+    // TODO: This will fail for large numbers
+    m.sizeX = (int) stream.readUnsignedInt();  // num columns (this is a column-major format)
+    m.sizeY = (int) stream.readUnsignedInt();  // num rows
+    long bytesPerRow = stream.readUnsignedInt();
+    long bytesPerImage = stream.readUnsignedInt();
+    stream.skipBytes(8);
+    dataOffset = stream.readUnsignedInt();
+    stream.skipBytes(16);
+    long bytesPerFrame = stream.readUnsignedInt();
+  }
 }
